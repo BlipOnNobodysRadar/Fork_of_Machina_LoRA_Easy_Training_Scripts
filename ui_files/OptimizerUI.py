b469b98d25f2f375ff3b11fde1356f87f51ea10e
67372a <117533205+67372a@users.noreply.github.com>
2024-10-29 08:07:41 -0500

    Expose FCompassPlus

--- ui_files/OptimizerUI.py
+++ ui_files/OptimizerUI.py
@@ -164,6 +164,7 @@ class Ui_optimizer_ui(object):
         self.optimizer_type_selector.addItem("")
         self.optimizer_type_selector.addItem("")
         self.optimizer_type_selector.addItem("")
+        self.optimizer_type_selector.addItem("")
         self.optimizer_type_selector.setObjectName(u"optimizer_type_selector")
         self.optimizer_type_selector.setFocusPolicy(Qt.StrongFocus)
 
@@ -453,6 +454,7 @@ class Ui_optimizer_ui(object):
         self.optimizer_type_selector.setItemText(26, QCoreApplication.translate("optimizer_ui", u"Ranger21", None))
         self.optimizer_type_selector.setItemText(27, QCoreApplication.translate("optimizer_ui", u"CompassPlus", None))
         self.optimizer_type_selector.setItemText(28, QCoreApplication.translate("optimizer_ui", u"CompassExperimental", None))
+        self.optimizer_type_selector.setItemText(29, QCoreApplication.translate("optimizer_ui", u"FCompassPlus", None))
 
 #if QT_CONFIG(tooltip)
         self.optimizer_type_selector.setToolTip(QCoreApplication.translate("optimizer_ui", u"<html><head/><body><p>Optimizer Type is the Optimizer that will be used during training</p></body></html>", None))
