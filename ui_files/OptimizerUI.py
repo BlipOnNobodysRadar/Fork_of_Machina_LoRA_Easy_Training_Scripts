a34b70c9b86b16407e6ad083043f4348c97448ab
67372a <117533205+67372a@users.noreply.github.com>
2024-11-07 18:44:16 -0600

    Expose SAM optimizer

--- ui_files/OptimizerUI.py
+++ ui_files/OptimizerUI.py
@@ -166,6 +166,7 @@ class Ui_optimizer_ui(object):
         self.optimizer_type_selector.addItem("")
         self.optimizer_type_selector.addItem("")
         self.optimizer_type_selector.addItem("")
+        self.optimizer_type_selector.addItem("")
         self.optimizer_type_selector.setObjectName(u"optimizer_type_selector")
         self.optimizer_type_selector.setFocusPolicy(Qt.StrongFocus)
 
@@ -457,6 +458,7 @@ class Ui_optimizer_ui(object):
         self.optimizer_type_selector.setItemText(28, QCoreApplication.translate("optimizer_ui", u"FCompassPlus", None))
         self.optimizer_type_selector.setItemText(29, QCoreApplication.translate("optimizer_ui", u"Lamb", None))
         self.optimizer_type_selector.setItemText(30, QCoreApplication.translate("optimizer_ui", u"Adan", None))
+        self.optimizer_type_selector.setItemText(31, QCoreApplication.translate("optimizer_ui", u"SAM", None))
 
 #if QT_CONFIG(tooltip)
         self.optimizer_type_selector.setToolTip(QCoreApplication.translate("optimizer_ui", u"<html><head/><body><p>Optimizer Type is the Optimizer that will be used during training</p></body></html>", None))
