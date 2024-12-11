4d3eb58ba3470834e26dea919a3d2e3083e660eb
67372a <117533205+67372a@users.noreply.github.com>
2024-12-11 14:06:29 -0600

    ADOPTNesterovScheduleFree

--- ui_files/OptimizerUI.py
+++ ui_files/OptimizerUI.py
@@ -178,6 +178,7 @@ class Ui_optimizer_ui(object):
         self.optimizer_type_selector.addItem("")
         self.optimizer_type_selector.addItem("")
         self.optimizer_type_selector.addItem("")
+        self.optimizer_type_selector.addItem("")
         self.optimizer_type_selector.setObjectName(u"optimizer_type_selector")
         self.optimizer_type_selector.setFocusPolicy(Qt.StrongFocus)
 
@@ -481,6 +482,7 @@ class Ui_optimizer_ui(object):
         self.optimizer_type_selector.setItemText(40, QCoreApplication.translate("optimizer_ui", u"FMARSCrop", None))
         self.optimizer_type_selector.setItemText(41, QCoreApplication.translate("optimizer_ui", u"ADOPTScheduleFree", None))
         self.optimizer_type_selector.setItemText(42, QCoreApplication.translate("optimizer_ui", u"ADOPTEMAMixScheduleFree", None))
+        self.optimizer_type_selector.setItemText(43, QCoreApplication.translate("optimizer_ui", u"ADOPTNesterovScheduleFree", None))
 
 #if QT_CONFIG(tooltip)
         self.optimizer_type_selector.setToolTip(QCoreApplication.translate("optimizer_ui", u"<html><head/><body><p>Optimizer Type is the Optimizer that will be used during training</p></body></html>", None))
