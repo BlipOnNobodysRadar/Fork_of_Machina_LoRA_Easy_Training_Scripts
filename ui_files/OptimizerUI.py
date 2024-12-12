51e3226836e4f0a8ba167e3ff51bb834b9d4cfe8
67372a <117533205+67372a@users.noreply.github.com>
2024-12-11 19:29:46 -0600

    cleanup, FADOPTScheduleFree

--- ui_files/OptimizerUI.py
+++ ui_files/OptimizerUI.py
@@ -179,6 +179,7 @@ class Ui_optimizer_ui(object):
         self.optimizer_type_selector.addItem("")
         self.optimizer_type_selector.addItem("")
         self.optimizer_type_selector.addItem("")
+        self.optimizer_type_selector.addItem("")
         self.optimizer_type_selector.setObjectName(u"optimizer_type_selector")
         self.optimizer_type_selector.setFocusPolicy(Qt.StrongFocus)
 
@@ -483,6 +484,7 @@ class Ui_optimizer_ui(object):
         self.optimizer_type_selector.setItemText(41, QCoreApplication.translate("optimizer_ui", u"ADOPTScheduleFree", None))
         self.optimizer_type_selector.setItemText(42, QCoreApplication.translate("optimizer_ui", u"ADOPTEMAMixScheduleFree", None))
         self.optimizer_type_selector.setItemText(43, QCoreApplication.translate("optimizer_ui", u"ADOPTNesterovScheduleFree", None))
+        self.optimizer_type_selector.setItemText(44, QCoreApplication.translate("optimizer_ui", u"FADOPTScheduleFree", None))
 
 #if QT_CONFIG(tooltip)
         self.optimizer_type_selector.setToolTip(QCoreApplication.translate("optimizer_ui", u"<html><head/><body><p>Optimizer Type is the Optimizer that will be used during training</p></body></html>", None))
