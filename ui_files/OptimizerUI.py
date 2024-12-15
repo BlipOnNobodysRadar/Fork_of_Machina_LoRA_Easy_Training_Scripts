f5a470aa3090109209d6c6afa859123bd1d10f4b
67372a <117533205+67372a@users.noreply.github.com>
2024-12-15 16:29:27 -0600

    RMSPropADOPT

--- ui_files/OptimizerUI.py
+++ ui_files/OptimizerUI.py
@@ -182,6 +182,7 @@ class Ui_optimizer_ui(object):
         self.optimizer_type_selector.addItem("")
         self.optimizer_type_selector.addItem("")
         self.optimizer_type_selector.addItem("")
+        self.optimizer_type_selector.addItem("")
         self.optimizer_type_selector.setObjectName(u"optimizer_type_selector")
         self.optimizer_type_selector.setFocusPolicy(Qt.StrongFocus)
 
@@ -489,6 +490,7 @@ class Ui_optimizer_ui(object):
         self.optimizer_type_selector.setItemText(44, QCoreApplication.translate("optimizer_ui", u"ADOPTMARSScheduleFree", None))
         self.optimizer_type_selector.setItemText(45, QCoreApplication.translate("optimizer_ui", u"FADOPTMARSScheduleFree", None))
         self.optimizer_type_selector.setItemText(46, QCoreApplication.translate("optimizer_ui", u"CompassADOPT", None))
+        self.optimizer_type_selector.setItemText(47, QCoreApplication.translate("optimizer_ui", u"CompassADOPT", None))
 
 #if QT_CONFIG(tooltip)
         self.optimizer_type_selector.setToolTip(QCoreApplication.translate("optimizer_ui", u"<html><head/><body><p>Optimizer Type is the Optimizer that will be used during training</p></body></html>", None))
